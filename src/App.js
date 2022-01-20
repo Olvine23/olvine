22ee09b760c502fc7312389feaf0a17f8e0a9389
Olvine23 <olivinegeorge@gmal.com>
2022-01-19 18:06:06 -0800

    rrr

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ const App = () => {
     return (
         <div className="app bg-gray-900">
         <div className="app-top">
-        <h1 className='text-green-500 mt-0 font-bold uppercase p-5 text-6xl font-poppins'>Vine <span className='text-red-500'> Lov </span></h1>
+        <h1 className='text-green-500 mt-0 font-bold uppercase p-2 text-6xl font-poppins'>Vine <span className='text-red-500'> Lov </span></h1>
+        <h1 className='text-white'>Made by Olvine: <a className='text-yellow-500' href="https://www.github.com/Olvine23" value="Github Profile">Link to Github</a></h1>
         </div>
             
         <div className="app-videos">
@@ -28,14 +29,20 @@ const App = () => {
         channel="Olvine George"
         avatarSrc={us}
         url={quote}
+        likes={1000}
+        shares={500}
         song="Juice Wrld - A thing about life" />
         <Video 
         channel="Olvine George"
         avatarSrc={us}
         url={juice}
+        likes={1000}
+        shares={500}
         song="Juice Wrld - Fast" />
         <Video className="alone" 
         avatarSrc={us}
+        likes={10000000}
+        shares={50000000}
         channel="Olvine George"
         url={alone}
         song="Juice Wrld - All Alone" />
