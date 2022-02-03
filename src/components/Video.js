344aabf21bbcaff3635f3b37060aa92565b1faa2
Olvine23 <olivinegeorge@gmal.com>
2022-02-03 12:08:59 -0800

    fixed it

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {useRef, useState, useEffect} from 'react'
 import './Video.css';
 import useElementOnScreen  from './useElementOnScreen'
